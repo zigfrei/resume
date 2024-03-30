f3eb30c073a0bfe801c331d6f2bf4b1151e68438
zigfrei <zigfrei.zf@gmail.com>
2024-03-30 11:58:01 +0300

    feat: add all printed text

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -25,8 +25,8 @@ export default function Loading(props: TLoading): ReactElement {
     "Done",
   ];
 
-  const typingDelay = 200;
-  const newTextDelay = 2000; // Delay between current and next 
+  const typingDelay = 50;
+  const newTextDelay = 1000; // Delay between current and next 
 
   function type() {
     console.log(lineText, charIndex);
@@ -36,6 +36,7 @@ export default function Loading(props: TLoading): ReactElement {
     }
   }
 
+  //5s deal
   useEffect(() => {
     if (props.loading) {
       const timeout = setTimeout(() => {
@@ -45,33 +46,143 @@ export default function Loading(props: TLoading): ReactElement {
     }
   }, [props.loading]);
 
+  //printing Loading ...
   useEffect(() => {
     if (props.loading && show == 1) {
       setTimeout(type, typingDelay);
       const timeout = setTimeout(() => {
         setShow(2);
         setCharIndex(0);
-        setPrintedText(1)
         setlineText('');
         setLineNumber(1);
-      }, 3000)
+      }, newTextDelay)
       return () => clearTimeout(timeout)
     }
   }, [lineText, props.loading, show]);
 
+  //printing Rendering a web page ...
   useEffect(() => {
     if (props.loading && show == 2) {
       setTimeout(type, typingDelay);
       const timeout = setTimeout(() => {
-        setShow(3)
-      }, 3000)
+        setShow(3);
+        setCharIndex(0);
+        setlineText('');
+        setLineNumber(2);
+      }, newTextDelay)
       return () => clearTimeout(timeout)
     }
   }, [lineText, props.loading, show]);
 
+  //printing Start to parse the HTML ...
+  useEffect(() => {
+    if (props.loading && show == 3) {
+      setTimeout(type, typingDelay);
+      const timeout = setTimeout(() => {
+        setShow(4);
+        setCharIndex(0);
+        setlineText('');
+        setLineNumber(3);
+      }, newTextDelay)
+      return () => clearTimeout(timeout)
+    }
+  }, [lineText, props.loading, show]);
+
+  //printing Fetch external resources ...
+  useEffect(() => {
+    if (props.loading && show == 4) {
+      setTimeout(type, typingDelay);
+      const timeout = setTimeout(() => {
+        setShow(5);
+        setCharIndex(0);
+        setlineText('');
+        setLineNumber(4);
+      }, newTextDelay)
+      return () => clearTimeout(timeout)
+    }
+  }, [lineText, props.loading, show]);
+
+  //printing Parse the CSS and build the CSSOM ...
+  useEffect(() => {
+    if (props.loading && show == 5) {
+      setTimeout(type, typingDelay);
+      const timeout = setTimeout(() => {
+        setShow(6);
+        setCharIndex(0);
+        setlineText('');
+        setLineNumber(5);
+      }, newTextDelay)
+      return () => clearTimeout(timeout)
+    }
+  }, [lineText, props.loading, show]);
+
+  //printing Execute the JavaScript ...
+  useEffect(() => {
+    if (props.loading && show == 6) {
+      setTimeout(type, typingDelay);
+      const timeout = setTimeout(() => {
+        setShow(7);
+        setCharIndex(0);
+        setlineText('');
+        setLineNumber(6);
+      }, newTextDelay)
+      return () => clearTimeout(timeout)
+    }
+  }, [lineText, props.loading, show]);
+
+    //printing Merge DOM and CSSOM to construct the render tree ...
+    useEffect(() => {
+      if (props.loading && show == 7) {
+        setTimeout(type, typingDelay);
+        const timeout = setTimeout(() => {
+          setShow(8);
+          setCharIndex(0);
+          setlineText('');
+          setLineNumber(7);
+        }, newTextDelay)
+        return () => clearTimeout(timeout)
+      }
+    }, [lineText, props.loading, show]);
+
+        //printing Calculate layout and paint ...
+        useEffect(() => {
+          if (props.loading && show == 8) {
+            setTimeout(type, typingDelay);
+            const timeout = setTimeout(() => {
+              setShow(9);
+              setCharIndex(0);
+              setlineText('');
+              setLineNumber(8);
+            }, newTextDelay)
+            return () => clearTimeout(timeout)
+          }
+        }, [lineText, props.loading, show]);
+
+                //printing Done
+                useEffect(() => {
+                  if (props.loading && show == 9) {
+                    setTimeout(type, typingDelay);
+                    const timeout = setTimeout(() => {
+                      // setShow(10);
+                      // setCharIndex(0);
+                      // setlineText('');
+                      // setLineNumber(9);
+                    }, newTextDelay)
+                    return () => clearTimeout(timeout)
+                  }
+                }, [lineText, props.loading, show]);
+
   return (
     <section className={`${styles.section} ${props.loading ? styles.loading : ''}`}>
-      {printedText>0 && <p className={styles.text}>Loading ...</p>}
+      {lineNumber > 0 && <p className={styles.text}>Loading ...</p>}
+      {lineNumber > 1 && <p className={styles.text}>Rendering a web page ...</p>}
+      {lineNumber > 2 && <p className={styles.text}>Start to parse the HTML ...</p>}
+      {lineNumber > 3 && <p className={styles.text}>Fetch external resources ...</p>}
+      {lineNumber > 4 && <p className={styles.text}>Parse the CSS and build the CSSOM ...</p>}
+      {lineNumber > 5 && <p className={styles.text}>Execute the JavaScript ...</p>}
+      {lineNumber > 6 && <p className={styles.text}>Merge DOM and CSSOM to construct the render tree ...</p>}
+      {lineNumber > 7 && <p className={styles.text}>Calculate layout and paint ...</p>}
+      {/* {lineNumber > 8 && <p className={styles.text}>Done</p>} */}
       <p className={styles.text}>{lineText}<span className={styles.cursor}>&nbsp;</span></p>
     </section>
   );
